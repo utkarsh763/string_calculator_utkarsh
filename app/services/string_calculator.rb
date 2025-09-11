7dff6fc6eb2d861aacb00f07c2b32c6d1e419c04
unknown <utkarsh763@gmail.com>
2025-09-10 23:45:23 -0700

    updated calculator code for new specs

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,19 +1,24 @@
-class StringCalculator 
-    def self.add(numbers)
-        return 0 if numbers.empty?
+class StringCalculator
+  def self.add(numbers)
+    return 0 if numbers.empty?
 
-        if numbers.start_with?("//")
-            delimiter, numbers = numbers.split("\n", 2)
-            delimiter = delimiter[2..]
-            num_list = numbers.split(delimiter).map(&:to_i)
-        else
-            delimiters = [",", "\n"]
-            num_list = numbers.split(Regexp.union(delimiters)).map(&:to_i)
-        end
+    delimiters = [",", "\n"]
+    if numbers.start_with?("//")
+      delimiter_section, numbers = numbers.split("\n", 2)
+      delimiter_section.slice!(0, 2) 
 
-        negatives = num_list.select { |n| n < 0}
-        raise "negative numbers not allowed #{negatives.join(',')}" if negatives.any?
-
-        num_list.sum
+      if delimiter_section.start_with?("[")
+        delimiters = delimiter_section.scan(/\[([^\]]+)\]/).flatten
+      else
+        delimiters = [delimiter_section]
+      end
     end
+
+    number_list = numbers.split(Regexp.union(delimiters)).map(&:to_i)
+
+    negatives = number_list.select { |n| n < 0 }
+    raise "negative numbers not allowed #{negatives.join(',')}" if negatives.any?
+
+    number_list.select { |n| n <= 1000 }.sum
+  end
 end
